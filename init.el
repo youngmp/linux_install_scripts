e65461f4689274931bfd798c8a266035352d4666
youngmp <ympark1988@gmail.com>
2015-08-31 01:37:07 -0400

    test initial project version. hello world!

---
+++ init.el
@@ -0,0 +1,7 @@
+; create soft link from here to ~/.emacs.d/init.el
+; cd to ~/.emacs.d, then enter
+; ln -s ~/Dropbox/linux/init.el ./init.el
+
+;(require 'ido)
+;(ido-mode t)
+
---
+++ pkg_install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# for new systems
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get -y -f install kile okular pdftk
+sudo apt-get -y -f install xppaut gimp vlc guake htop
+sudo apt-get -y -f install python-numpy python-scipy python-matplotlib ipython r-base
+sudo apt-get -y -f install emacs pyflakes pymacs
+sudo apt-get -y -f install openjdk-6-jre
+sudo apt-get -y -f install vncviewer
+sudo apt-get -y -f install git
+git config --global user.name "youngmp"
+git config --global user.email "ympark1988@gmail.com"
+git config --global core.editor emacs
+
+sudo apt-get autoremove
+sudo apt-get clean
+
+# get dropbox and google chrome
+
+# fun stuff
+sudo apt-get -y -f install cmatrix
+
+# Julia setup
+#sudo add-apt-repository ppa:staticfloat/juliareleases
+#sudo add-apt-repository ppa:staticfloat/julia-deps
+#sudo apt-get update
+#sudo apt-get install julia
+
+sudo apt-get -y -f autoremove
