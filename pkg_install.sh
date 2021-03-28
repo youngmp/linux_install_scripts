3e0f4fefc24044df84fc4bbd309161a642ac7872
Youngmin Park <ympark1988@gmail.com>
2021-03-28 12:36:47 -0400

    changed apt-get to apt

--- pkg_install.sh
+++ pkg_install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 # for new systems
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt -y update
+sudo apt -y upgrade
 
-sudo apt-get -y -f install kile okular pdftk
-sudo apt-get -y -f install xppaut gimp vlc guake htop
-sudo apt-get -y -f install python3-numpy python3-scipy python3-matplotlib ipython r-base
-sudo apt-get -y -f install emacs pyflakes pymacs
-sudo apt-get -y -f install openjdk-6-jre
-sudo apt-get -y -f install vncviewer
-sudo apt-get -y -f install git
+sudo apt -y -f install kile okular pdftk
+sudo apt -y -f install xppaut gimp vlc guake htop
+sudo apt -y -f install python3-numpy python3-scipy python3-matplotlib ipython r-base
+sudo apt -y -f install emacs pyflakes pymacs
+sudo apt -y -f install openjdk-6-jre
+sudo apt -y -f install vncviewer
+sudo apt -y -f install git
 git config --global user.name "youngmp"
 git config --global user.email "ympark1988@gmail.com"
 git config --global core.editor emacs
 
-sudo apt-get autoremove
-sudo apt-get clean
+sudo apt autoremove
+sudo apt clean
 
 # get dropbox and google chrome
 
 # fun stuff
-sudo apt-get -y -f install cmatrix
+sudo apt -y -f install cmatrix
 
 # Julia setup
 #sudo add-apt-repository ppa:staticfloat/juliareleases
@@ -29,6 +29,6 @@ sudo apt-get -y -f install cmatrix
 #sudo apt-get update
 #sudo apt-get install julia
 
-sudo apt-get -y -f autoremove
+sudo apt -y -f autoremove
 
 # test update
